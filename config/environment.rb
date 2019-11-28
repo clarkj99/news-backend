b9d97e8b7421fda243ff25a5c45174f78c849a63
clarkj99 <clark@stontech.net>
2019-11-28 02:19:01 -0500

    api seeds article model

---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+# Load the Rails application.
+require_relative "application"
+require_relative "config"
+
+# Initialize the Rails application.
+Rails.application.initialize!
---
+++ db/migrate/20191128053240_create_articles.rb
@@ -0,0 +1,17 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :source
+      t.string :source_name
+      t.string :author
+      t.string :title
+      t.string :description
+      t.string :url
+      t.string :url_to_image
+      t.date :published_at
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
